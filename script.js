06a730183b278beb7fda7447053ae06bacd7b319
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-15 11:06:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let cost=0;
+	arr.sort((a,b)=>a-b);
+	while (arr.length > 1) {
+		const newarr=arr.shift()+arr.shift();
+		cost+=newarr;
+		arr.push(newarr);
+		arr.sort((a,b)=>a-b);
+	}
+	return cost;
   
 }
+cosole.log(mincost([4,3,2,6]);
 
 module.exports=mincost;
