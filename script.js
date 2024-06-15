86d564a87897e5586a6e1c715b2fbf97b17bacb6
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-15 16:14:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-	let cost=0;
-	arr.sort((a,b)=>a-b);
-	while (arr.length > 1) {
-		const newarr=arr.shift()+arr.shift();
-		cost+=newarr;
-		arr.push(newarr);
-		arr.sort((a,b)=>a-b);
-	}
-	return cost;
-  
+function mincost(arr) {
+    let cost = 0;
+    arr.sort((a, b) => a - b);
+    while (arr.length > 1) {
+        const newRope = arr.shift() + arr.shift();
+        cost += newRope;
+        arr.push(newRope);
+        arr.sort((a, b) => a - b); // This sort can be inefficient for large arrays
+    }
+    return cost;
 }
-cosole.log(mincost([4,3,2,6]);
-console.log([1,2,3,4,5]);
 
-module.exports=mincost;
+console.log(mincost([4, 3, 2, 6])); // Output should be 29
+console.log(mincost([1, 2, 3, 4, 5])); // Output should be 33
+
+module.exports = mincost;
